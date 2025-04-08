a7251def26d9ec82e30bf4c53c111fc40bb1fcb0
Andresmejia62 <amejia@uamv.edu.ni>
2025-04-08 08:57:21 -0600

    Semana4

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#declarar variables
+nombre_completo= "Andres Mejia"
+edad = 17
+altura = 1.70
+soltero = True
+
+print(f"Nombre completo:{nombre_completo}")
+print(f"Tipo de dato:{type(nombre_completo)}")
+print(f"Edad:{edad}")
+print(f"Tipo de dato:{type(edad)}")
+print(f"Altura:{altura}")
+print(f"Tipo de dato:{type(altura)}")
+print(f"Estado civil:{soltero}")
+print(f"Tipo de dato:{type(soltero)}")
---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+#tipo de datos numerico 
+
+cantidad = 10 
+precio = 130 
+total = cantidad * precio
+
+print(f"{"Cantidad:>10"} {cantidad:>6}")
+print(f"{ftexto("Precio:")}{precio:>6}")
+print(f"{ftexto("Total:")}{total:>6}")
