cf9890270122e23cca4709b854f336c832c8faab
Andrés Mejía <amejia@uamv.edu.ni>
2025-04-10 16:39:41 -0600

    Update ejercicio1.py

--- ejercicios_nomenclatura/ejercicio1.py
+++ ejercicios_nomenclatura/ejercicio1.py
@@ -9,6 +9,3 @@ print("snake_case: total_amount")
 
 # SCREAMING_SNAKE_CASE
 print("SCREAMING_SNAKE_CASE:  TOTAL_AMOUNT")
-
-# kebab-case (no es válido en Python, se muestra solo como referencia)
-# total-amount = 0  # Esto generaría un error de sintaxis
